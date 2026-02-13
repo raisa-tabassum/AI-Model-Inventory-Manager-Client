2e34731a3965b66f8a196229f2ddcfe2639390a9
Raisa Tabassum <raisatabassume@gmail.com>
2026-02-13 19:38:11 +0600

    added Toaster

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import "./index.css";
 import { router } from "./router/routes";
 import { RouterProvider } from "react-router";
 import AuthProvider from "./context/AuthProvider";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
       <RouterProvider router={router} />,
     </AuthProvider>
+    <Toaster position="top-right"></Toaster>
   </StrictMode>,
 );
